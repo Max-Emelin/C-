98fc7c44ae565d5070cccc89666f71d1987e16c4
емелин макс <maxemelinwhynot@mail.ru>
2024-10-24 12:07:29 +0400

    Добавлена авторизация и регистрация.

--- NewProjects/RunGroopWebApp/Models/AppUser.cs
+++ NewProjects/RunGroopWebApp/Models/AppUser.cs
@@ -11,7 +11,7 @@ namespace RunGroopWebApp.Models
         public int? Distance { get; set; }
 
         [ForeignKey("Address")]
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
 
         public Address? Address { get; set; }
 
